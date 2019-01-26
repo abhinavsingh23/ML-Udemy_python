4c8f83bad349fc189df230868f907350dc17e4e2
abhinavsingh23 <abhinavsingh2902@gmail.com>
2019-01-26 08:12:21 +0530

    regression

--- Simple-Linear-Regression/simple-linear-regression.py
+++ Simple-Linear-Regression/simple-linear-regression.py
@@ -5,10 +5,10 @@ import pandas as pd
 dataset = pd.read_csv('Salary_Data.csv')
 
 X= dataset.iloc[:,:-1].values
-dfX = pd.DataFrame(X)
+
 
 y= dataset.iloc[:,1].values
-dfy = pd.DataFrame(y)
+
 
 # Splitting
 
@@ -16,14 +16,29 @@ from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X,y, test_size = 1/3, random_state = 0)
 
 
+# Fitting data to algo
 
-"""----------------------------------------------------------
-# Feature Scaling
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
 
-from sklearn.preprocessing import StandardScaler
-sc_X = StandardScaler()
-X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.transform(X_test)
+# Predicting test set result
+y_pred = regressor.predict(X_test)
 
----------------------------------------------------------"""
 
+# Plotting results
+plt.scatter(X_train, y_train , color= 'red')
+plt.plot(X_train, regressor.predict(X_train), color= 'blue')
+plt.title('Salary vs Exp (Training set)')
+plt.xlabel('Years of Exp')
+plt.ylabel('Salary')
+plt.show()
+
+
+# Plotting test results
+plt.scatter(X_test, y_test , color= 'red')
+plt.plot(X_train, regressor.predict(X_train), color= 'blue')
+plt.title('Salary vs Exp (Test set)')
+plt.xlabel('Years of Exp')
+plt.ylabel('Salary')
+plt.show()
