177c36b69ffeee664d992797d5f13f21aa275094
abhinavsingh23 <abhinavsingh2902@gmail.com>
2019-06-27 23:38:27 +0530

    changes

--- Data Preprocessing/data_preprocessing_template.py
+++ Data Preprocessing/data_preprocessing_template.py
@@ -1,40 +1,8 @@
+# Importing libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-dataset = pd.read_csv('Salary_Data.csv')
 
-X= dataset.iloc[:,:-1].values
-dfX = pd.DataFrame(X)
-
-y= dataset.iloc[:,1].values
-dfy = pd.DataFrame(y)
-
-
-
-
-
-# Splitting
-
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X,y, test_size = 1/3, random_state = 0)
-
-
-
-"""----------------------------------------------------------
-# Feature Scaling
-
-from sklearn.preprocessing import StandardScaler
-sc_X = StandardScaler()
-X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.transform(X_test)
-
----------------------------------------------------------"""
-
-
-
-
-
-
-
+# Importing Dataset
 
